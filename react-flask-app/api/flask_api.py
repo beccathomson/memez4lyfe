10700f5e48c2553ce7122a89bcf39574a85e70ca
becca <beccabthomson@gmail.com>
2020-09-19 16:14:17 -0700

    api stuff

--- react-flask-app/api/flask_api.py
+++ react-flask-app/api/flask_api.py
@@ -1,25 +1,25 @@
+import csv
 import pickle
 import time
 from flask import Flask, request
 
 app = Flask(__name__)
-meme_urls = {'Grumpy Cat': 'grumpycaturl'}
+
 
 @app.route('/time')
 def get_current_time():
 	return {'time': time.time()}
 
 
-@app.route('/meme', methods=["GET", "POST"])
+@app.route('/meme', methods=["GET"])
 def get_meme_image_url():
+	return {'meme_url': 'url_from_backend'}
 	input_caption = request.args.get('caption')
-	return {'input_caption': input_caption}
-	# input_caption = ["i am an example caption"]
 	vectorizer = pickle.load(open("vectorizer.pickle", "rb"))
 	loaded_model = pickle.load(open("classifier.pickle", "rb"))
 	label_encoder = pickle.load(open('label_encoder.pickle', 'rb'))
 	input_caption = vectorizer.transform(input_caption)
 	meme_code = loaded_model.predict(input_caption)
 	meme_name = label_encoder.inverse_transform([int(meme_code)])
-	return {'meme_image_url': meme_urls['Grumpy Cat'], 'input_caption': input_caption}
+	return {'meme_url': 'url_from_backend'}
 
--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -6,29 +6,14 @@ function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [memeUrl, setMemeUrl] = useState(0);
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
 
   useEffect(() => {
-    fetch('/meme?caption=\'input sample caption\'').then(res => res.json()).then(data => setMemeUrl(data.input_caption));
+    fetch('/meme?caption=\'input sample caption\'').then(res => res.json()).then(data => setMemeUrl(data.meme_url));
   }, []);
 
   return (
     <div className="Memez4Lyfe">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-          <p>The current time is {currentTime}.</p>
           <p>The meme url is {memeUrl}.</p>
       </header>
     </div>
